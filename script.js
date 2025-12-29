63afa561ec8570cac57c4f4738b068d003ed39ad
QuanCHESS♟️ <dnanhquanyd1@gmail.com>
2025-12-29 19:53:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-function revealOnScroll() {
-    const elements = document.querySelectorAll('.reveal');
+function reveal() {
+    const reveals = document.querySelectorAll(".reveal");
     const windowHeight = window.innerHeight;
-    elements.forEach(el => {
+
+    reveals.forEach(el => {
         const top = el.getBoundingClientRect().top;
-        if (top < windowHeight - 80) {
-            el.classList.add('active');
+        if (top < windowHeight - 100) {
+            el.classList.add("active");
         }
     });
 }
 
-window.addEventListener('scroll', revealOnScroll);
-revealOnScroll();
+window.addEventListener("scroll", reveal);
+reveal();
