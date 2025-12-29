3ac330bf4c00056bfb8d2bc04f3f1f7ecc1ba5f3
QuanCHESS♟️ <dnanhquanyd1@gmail.com>
2025-12-29 11:50:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-function sayHello() {
-    alert("Xin chào! Bạn đã chạy JavaScript thành công 🎉");
+function revealOnScroll() {
+    const reveals = document.querySelectorAll(".reveal");
+
+    for (let i = 0; i < reveals.length; i++) {
+        const windowHeight = window.innerHeight;
+        const elementTop = reveals[i].getBoundingClientRect().top;
+        const revealPoint = 100;
+
+        if (elementTop < windowHeight - revealPoint) {
+            reveals[i].classList.add("active");
+        }
+    }
 }
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
